1f3c8b5d74a46c6fa5f7fb4d45b5096e592a62a5
d.delcastillo <danieldelcastillo97@hotmail.com>
2021-04-19 00:50:28 -0500

    Optimizaciones a ciclos.

--- MP3/E2.py
+++ MP3/E2.py
@@ -1,5 +1,4 @@
 # %% Packages
-import numpy
 import numpy as np
 
 
@@ -14,13 +13,14 @@ def MyConnComp_201630945_201622695(binary_image, conn):
         raise Exception('Connectivity value must be 4 or 8.')
     b = np.asarray([[0, 1, 0], [1, 1, 1], [0, 1, 0]]) if conn == 4 else np.ones([3, 3], dtype=np.int_)
     n, m = np.size(binary_image, 0), np.size(binary_image, 1)
-    v = np.sum(binary_image)  # Number of vertexes.
+    v = np.sum(binary_image)  # Number of vertexes (pixels).
     coord_to_vertex = {}
     # Base UnionFinder
     par = [-1] * v  # All pixels are isolated to begin with (they're their own component).
     num_components = v  # Number of components.
 
     # The root determines which component does the pixel (box) belongs to.
+    # Designed so that a vertex's root updates whenever it's called.
     def root(p_vertex):
         if par[p_vertex] < 0:
             return p_vertex
@@ -39,6 +39,7 @@ def MyConnComp_201630945_201622695(binary_image, conn):
     for i, j in coord_to_vertex.keys():
         for index_x, x in enumerate(range(i - 1, i + 2)):
             for index_y, y in enumerate(range(j - 1, j + 2)):
+                # Checking if (x,y) is not outside the image, that (i,j) != (x,y), and that they're connected.
                 if 0 <= x < n and 0 <= y < m and not (x == i and y == j) and b[index_x, index_y]:
                     if binary_image[x, y]:
                         # What happens here is a merge of components when connected (same root).
@@ -54,12 +55,14 @@ def MyConnComp_201630945_201622695(binary_image, conn):
                             num_components -= 1  # One fewer component since two merged.
     # Making each connected components based on the parents.
     labeled_image = np.zeros([n, m], dtype=np.int_)
-    parents = np.zeros(v, dtype=numpy.int_)
+    parents = np.zeros(v, dtype=np.int_)
+    parent_to_index = {}
+    c = 0
     for i in range(v):
         parents[i] = root(i)
-    parent_to_index = {}
-    for index, i in enumerate(set(parents)):
-        parent_to_index[i] = index
+        if parents[i] not in parent_to_index:
+            parent_to_index[parents[i]] = c
+            c += 1
     # Each pixel is re-drawn with an unique value based on it's respective connected component.
     pixel_labels = [[] for _ in range(num_components)]
     for (x, y), vertex in coord_to_vertex.items():
@@ -71,4 +74,4 @@ def MyConnComp_201630945_201622695(binary_image, conn):
 
 # %%
 test = np.asarray([[1, 0, 0], [0, 0, 1], [1, 1, 0]])
-image, labels = MyConnComp_201630945_201622695(test, 8)
+image, labels = MyConnComp_201630945_201622695(test, 4)
